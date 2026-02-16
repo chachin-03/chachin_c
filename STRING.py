71dd139ff62252b6b5260965815fbaef3a6c1962
chachin-03 <814725243028@trp.srmtrichy.edu.in>
2026-02-16 14:36:35 +0530

    STRING.py

---
+++ STRING.py
@@ -0,0 +1,11 @@
+s1 = "python"
+s2 = "programming"
+print("string 1 is:",s1)
+print("string 2 is:, s2")
+print("concentration:", s1+s2)
+print("concentration :", s1 +" "+ s2)
+print("length of the string 1 is :", len(s1))
+print("length of the string 2 is :", len(s2))
+print("first letter of the string 1 :", s1[0])
+print("last letter of the string 1 :", s1[-1])
+print("substring of s1 :", s1[1:3])
